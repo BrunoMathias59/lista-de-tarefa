0abd4ee40a0b4b0a6b3cc8bc615339a0c21eea66
BrunoMathias59 <bruninho.mathias@hotmail.com>
2025-04-04 20:23:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function Coletarcep() {
+  let cep = document.getElementById("cep").value;
+  console.log(cep);
+  dados(cep);
+}
+async function dados(cep) {
+ let dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => response.json());
+ console.log(dados)
+ dadosTela(dados) 
+
+  
+}
+
+function dadosTela(dados) {
+  document.querySelector("#endereco").innerHTML = dados.logradouro;
+  document.querySelector("#bairro").innerHTML = dados.bairro;
+  document.querySelector("#cidade").innerHTML = dados.localidade;
+  document.querySelector("#estado").innerHTML = dados.estado;
+  document.querySelector("#regiao").innerHTML = dados.regiao;
+}
